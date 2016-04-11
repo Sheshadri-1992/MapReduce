bdb52311fc69d90a9887b045ef5fb03baf35bf44
Shweta Verma <cool.shwetav@gmail.com>
2016-04-11 16:40:44 +0530

    Client-Constant added. Implemented get hdfs

--- src/com/mapreduce/client/ClientDriver.java
+++ src/com/mapreduce/client/ClientDriver.java
@@ -1,10 +1,135 @@
 package com.mapreduce.client;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.mapreduce.jobtracker.IJobTracker;
+import com.mapreduce.misc.MapReduce.JobStatusRequest;
+import com.mapreduce.misc.MapReduce.JobStatusResponse;
+import com.mapreduce.misc.MapReduce.JobSubmitRequest;
+import com.mapreduce.misc.MapReduce.JobSubmitResponse;
+
 public class ClientDriver {
 
+	public static String mapName;
+	public static String reducerName;
+	public static String inputFile;
+	public static String outputFile;
+	public static Integer numReducers;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
 
+//		<mapName> <reducerName> <inputFile in HDFS> <outputFile in HDFS> <numReducers>
+		
+		mapName = args[0];
+		reducerName  = args[1];
+		inputFile = args[2];
+		outputFile = args[3];
+		numReducers = Integer.parseInt(args[4]);
+		
+		
+		try {
+			submitJob();
+		} catch (RemoteException | InvalidProtocolBufferException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	private static void submitJob() throws AccessException, RemoteException, NotBoundException, InvalidProtocolBufferException {
+		// TODO Auto-generated method stub
+		
+		Registry registry = getRegistry();
+		
+		if(registry!=null)
+		{
+			IJobTracker jtStub;
+			jtStub=(IJobTracker) registry.lookup(Constants.JOB_TRACKER_IP);  // @Sheshadri : Name kept for binding
+			
+			JobSubmitRequest.Builder jobSubmitRequest = JobSubmitRequest.newBuilder();
+			
+			byte[] responseArray = jtStub.jobSubmit(jobSubmitRequest.build().toByteArray());
+			JobSubmitResponse jobSubmitResponse = JobSubmitResponse.parseFrom(responseArray);
+			int jobId =jobSubmitResponse.getJobId();
+			
+			
+			JobStatusRequest.Builder jobStatusRequest = JobStatusRequest.newBuilder();
+			jobStatusRequest.setJobId(jobId);
+			
+			
+			while(true)
+			{
+				byte[] statusResponseArr = jtStub.getJobStatus(jobStatusRequest.build().toByteArray());
+				
+				JobStatusResponse jobStatusResponse = JobStatusResponse.parseFrom(statusResponseArr);
+				
+				if(jobStatusResponse.getJobDone())
+				{
+					break;
+				}else
+				{
+					System.out.println("Print Status");
+				}
+				
+				try {
+					Thread.sleep(Constants.JOB_STATUS_FREQ);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			System.out.println("Operation completed");
+			
+			/**
+			 *  Read contents of output file after this.
+			 *  Implement hdfs read in misc;
+			 */
+			
+		}
+		
+		
+		
+	}
+		
+		
+	private static  Registry getRegistry()
+	{
+		BufferedReader buff;
+		String line="";
+		try {
+			buff = new BufferedReader(new FileReader(Constants.JT_CONF_FILE));  
+			line=buff.readLine();
+			buff.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		String tokens[] = line.split(" ");
+		
+		Registry registry =null;
+		try {
+			registry = LocateRegistry.getRegistry(tokens[0],Integer.parseInt(tokens[1]));
+			
+		}catch(RemoteException e)
+		{
+			
+		}
+		
+		return registry;
 	}
 
 }
---
+++ src/com/mapreduce/client/Constants.java
@@ -0,0 +1,10 @@
+package com.mapreduce.client;
+
+public final class Constants {
+
+
+	public static final String  JT_CONF_FILE = "JTConf";
+	public static final String  JOB_TRACKER_IP = "JobTracker";
+	public static final long JOB_STATUS_FREQ = 5000;
+	public static final int BLOCK_SIZE=1000;// 32 MB
+}
